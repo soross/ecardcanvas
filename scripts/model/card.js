ddbc0b522da5d37a83a81eb75133844d0098f5fa
soross <pgyh@hotmail.com>
2016-12-27 10:49:56 +0800

    Update card.js

--- scripts/model/card.js
+++ scripts/model/card.js
@@ -194,7 +194,7 @@
         for (var i = 0; i < this.items.length; ++i) {
             cardConfig[cardConfig.length] = this.items[i].getConfig();
         }
-        return JSON.stringify(cardConfig);
+        return cardConfig;
     },
     getDescription: function () {
         var desc = "";
