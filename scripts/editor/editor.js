0ad4cf51e155573c295429b5f0f64a58fb0f9750
soross <pgyh@hotmail.com>
2016-12-29 17:15:08 +0800

    Update editor.js

--- scripts/editor/editor.js
+++ scripts/editor/editor.js
@@ -104,15 +104,15 @@ $(function () {
 
     function initilize() {
         var cardConfigg = JSON.parse(resumex);
-	var cardConfig = JSON.parse(cardConfigg);
+	
         // If this is a reload try to load card config from local storage
         var hash = window.location.hash;
         var restore = (hash == "#edit" || hash == "#save");
 	    
 	    
-	alert("data isbb9 : " + JSON.stringify(cardConfigg));
-	alert("data isbb79 : " + cardConfigg);   
-	    
+	alert("data isbbmm9 : " + JSON.stringify(cardConfigg));
+	alert("data isbbmm79 : " + cardConfigg);   
+        var cardConfig = JSON.parse(cardConfigg); 
         alert("data is9 : " + JSON.stringify(cardConfig));
 	alert("data is79 : " + cardConfig);
         // we add this to the end of url so if user reloads it we load it from local storage
